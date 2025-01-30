211fb6b76afb12c9e118777e2412b092858a5676
imran8r <imranhrrabbi@gmail.com>
2025-01-31 00:40:59 +0600

    Add files via upload

--- Basics.py/testtask.py
+++ Basics.py/testtask.py
@@ -1,6 +1,40 @@
+#POEM
+
 a='twinkle twinkle little stars'
+print(a)
+
+#REMAINDER
 x=int(input('value of x:'))
 y=int(input('value of y:'))
 z=x%y
-print(a)
-print(z)
+
+print("the remainder is",z)
+
+#AVERAGE NUMBER
+q=float(input('number?'))
+w=float(input('number?')) 
+r=float(input('number?')) 
+avg=(a+b+c)/3
+
+print('the average is',avg)
+
+
+#FRUITLIST
+f=input('fruit name?')
+g=input('fruit name?')
+h=input('fruit name?')
+i=input('fruit name?')
+j=input('fruit name?')
+k=input('fruit name?')
+l=input('fruit name?')
+li=[f,g,h,i,j,k,l]
+
+
+#COUNTZERO
+
+m=[1,0,0,1,0,0,2,0,3,-5,0,0]
+cnt= m.count(0)
+print('zeros in the list:',cnt)
+
+
+
